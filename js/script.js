72c78737f4aa261fa81f5f0ed9565cbf34172ceb
Francisco Vaz <franciscovaz@MacBook-Pro-de-Francisco.local>
2020-01-27 23:02:16 +0000

    responsive, skills, projects and articles

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function(){
+    'use strict';
+
+    $('ul.filters > li').on('click', function(e){
+        e.preventDefault();
+    
+        $('ul.filters > li').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    
+
+})(jQuery);
+
+
